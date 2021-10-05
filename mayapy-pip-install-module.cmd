9c9a30c280a8ebba2c61a826da21750418a1d11d
m3trik <m3trik@live.com>
2021-10-05 14:29:05 -0400

    -

--- mayapy-pip-install-module.cmd
+++ mayapy-pip-install-module.cmd
@@ -6,8 +6,11 @@
 ECHO Give the name of the module to install:
 set /p MODULE=
 ECHO/
+ECHO Maya version? (ie. 2020):
+set /p VERSION=
+ECHO/
 ECHO Verify the path to mayapy.exe and hit enter to install:
-set /p MAYAPY_PATH="C:\Program Files\Autodesk\Maya2020\bin\mayapy.exe"
+set /p MAYAPY_PATH="C:\Program Files\Autodesk\Maya%VERSION%\bin\mayapy.exe"
 
 
 %MAYAPY_PATH% pip install %MODULE%
