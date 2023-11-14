03db74342abe4917bb316de43eb371bd8e835607
m3trik@live.com <m3trik@live.com>
2023-11-14 11:57:24 -0500

    -

--- mayapy-package-manager.bat
+++ mayapy-package-manager.bat
@@ -1,15 +1,12 @@
 @ECHO off
+:: Maya Python Package Manager
 :: pip needs to already have been installed to mayapy.
-:: the following commands are for a windows environment.
-
-:: Admin check
-rem fltmc >nul 2>nul || set _=^"set _ELEV=1^& cd /d """%cd%"""^& "%~f0" %* ^"&&((if "%_ELEV%"=="" ((powershell -nop -c start cmd -args '/d/x/s/v:off/r',$env:_ -verb runas >nul 2>nul) || (mshta vbscript:execute^("createobject(""shell.application"").shellexecute(""cmd"",""/d/x/s/v:off/r ""&createobject(""WScript.Shell"").Environment(""PROCESS"")(""_""),,""runas"",1)(window.close)"^) >nul 2>nul)))& exit /b)
-
+:: The following commands are for a Windows environment.
 
 :intro
 color 07
-set ver=0.5.0
-ECHO Welcome to Maya Python Package Manager v%ver%
+set ver=0.5.1
+powershell -Command "Write-Host 'Maya Python Package Manager v%ver%' -ForegroundColor Cyan"
 ECHO\
 
 
@@ -17,28 +14,32 @@ ECHO\
 ECHO What is your Maya version? (ex. 2022)
 set /p maya_version=
 set mayapy="%ProgramFiles%\Autodesk\Maya%maya_version%\bin\mayapy.exe"
-echo %mayapy%
+echo  Using Python interpreter at %mayapy%
 
 
 :main
 ECHO\
 ECHO What do you want to do?
-ECHO	1- Install package
-ECHO	2- Uninstall package
-ECHO	3- List installed packages
-ECHO	4- Update existing packages
-ECHO	5- Get package info
-ECHO	6- Backup
-ECHO	7- Restore
+ECHO	1. Install package
+ECHO	2. Update package
+ECHO	3. Uninstall package
+ECHO	4. Get package info
+ECHO	5. List installed packages
+ECHO	6. Give Write Permission
+ECHO	7. Backup
+ECHO	8. Restore
+ECHO	9. Exit
 
-CHOICE /C:1234567
+CHOICE /C:123456789
 
-IF ERRORLEVEL 7 goto restore
-IF ERRORLEVEL 6 goto backup
-IF ERRORLEVEL 5 goto info
-IF ERRORLEVEL 4 goto update
-IF ERRORLEVEL 3 goto list
-IF ERRORLEVEL 2 goto uninstall
+IF ERRORLEVEL 9 goto end
+IF ERRORLEVEL 8 goto restore
+IF ERRORLEVEL 7 goto backup
+IF ERRORLEVEL 6 goto admin
+IF ERRORLEVEL 5 goto list
+IF ERRORLEVEL 4 goto info
+IF ERRORLEVEL 3 goto uninstall
+IF ERRORLEVEL 2 goto update
 IF ERRORLEVEL 1 goto install
 
 
@@ -69,21 +70,9 @@ goto main
 :update
 ECHO\
 ECHO Which package do you want to UPDATE?
-rem ECHO `all` will attempt to update all packages
-rem ECHO `pip` will update the package installer
-rem ECHO or give the name of a specific package:
 set /p module=
 ECHO\
-if "%module%"=="all" (
-:: update all on windows:
-%mayapy% -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U
-:: update all on unix:
-rem %mayapy% -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
-) else if "%module%"=="pip" (
-%mayapy% -m pip install --upgrade pip
-) else (
 %mayapy% -m pip install %module% --upgrade
-)
 goto main
 
 
@@ -107,5 +96,30 @@ goto main
 
 :restore
 ECHO\
-%mayapy% -m pip install -r requirements.txt
+IF NOT EXIST requirements.txt (
+    powershell -Command "Write-Host 'requirements.txt not found. Please ensure it exists in the current directory.' -ForegroundColor Red"
+    goto main
+)
+
+ECHO The following packages will be restored:
+ECHO -----------------------------------
+powershell -Command "Get-Content requirements.txt | Write-Host -ForegroundColor Green"
+ECHO -----------------------------------
+ECHO\
+ECHO Do you want to proceed with the restore operation? (Y/N)
+set /p user_choice=
+IF /I "%user_choice%"=="Y" (
+    %mayapy% -m pip install -r requirements.txt
+) ELSE (
+    ECHO Restore operation cancelled.
+)
 goto main
+
+
+:admin
+fltmc >nul 2>nul || set _=^"set _ELEV=1^& cd /d """%cd%"""^& "%~f0" %* ^"&&((if "%_ELEV%"=="" ((powershell -nop -c start cmd -args '/d/x/s/v:off/r',$env:_ -verb runas >nul 2>nul) || (mshta vbscript:execute^("createobject(""shell.application"").shellexecute(""cmd"",""/d/x/s/v:off/r ""&createobject(""WScript.Shell"").Environment(""PROCESS"")(""_""),,""runas"",1)(window.close)"^) >nul 2>nul)))& exit /b)
+goto main
+
+
+:end
+ECHO Exiting...
